4da18e0e027605574062c1502e972b2c36ab3301
anatoliy pereguda <pereguda.av@proton.me>
2024-08-04 16:48:43 +0500

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+    n = Math.abs(n);
+    let str = String(n);
+    //console.log(str);
+    //console.log(typeof str);
+    let res = "";
+    for (let i = 0; i < str.length; i += 1) {
+        //console.log(str[i]);
+        res = str[i] + res;
+        //console.log(res);
+    }
+    return Number(res);
 }
